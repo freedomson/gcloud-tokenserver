9bfd7077eb0626555a2d6e777f48f8782186d3e8
Philip Almeida <>
2018-06-02 02:04:30 +0100

    Token server test 1

--- server.js
+++ server.js
@@ -1,25 +1,50 @@
+//  OpenShift sample Node application
+var express = require('express'),
+    app     = express(),
+    morgan  = require('morgan');
+    
+Object.assign=require('object-assign')
+
+app.engine('html', require('ejs').renderFile);
+app.use(morgan('combined'))
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
 //Load HTTP module
 var http = require("http");
-var dialogflow = require("dialogflow");
-//Create HTTP server and listen on port 8000 for requests
-http.createServer(function (request, response) {
 
-   // Set the response HTTP header with HTTP status and Content type
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+app.get('/', function (req, res) {
+  res.send('no op');
+});
+app.get('/gettoken', function (req, res) {
+  //auth.authorizeRequest({/*...*/}, function (err, authorizedReqOpts) {});
+  auth.getToken(function (err, token) { 
+    if (req.headers.token!==process.env.APP_CODE)
+      res.status(500).send('Something bad happened!'); 
+    else
+      res.json({"token": token});
+  });
+  
+});
 
-   // Send the response body "Hello World"
-   response.end('Hello World\n');
-}).listen(8000);
+// error handling
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
+});
 
-// Print URL for accessing server
-console.log('Server running at http://127.0.0.1:8000/')
-	
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app ;
+
   var googleAuth = require('google-auto-auth');
  
   var authConfig = {};
    
   // path to a key:
-  authConfig.keyFilename = '/Users/freedomson/projects/joao/tokenserver/key.json';
+  authConfig.keyFilename = 'key.json';
    /*
   // or a credentials object:
   authConfig.credentials = {
@@ -29,6 +54,3 @@ console.log('Server running at http://127.0.0.1:8000/')
   authConfig.scopes = ['https://www.googleapis.com/auth/cloud-platform']
   // Create a client
   var auth = googleAuth(authConfig);
-   
-  //auth.authorizeRequest({/*...*/}, function (err, authorizedReqOpts) {});
-  auth.getToken(function (err, token) { console.log(token,err)});
